85b1bef8a86bc7c2244e323c5bd3f7583626bca7
Bohdan Terskow <bohdanterskow@gmail.com>
2023-11-05 22:38:35 +0200

    updated api

--- credentials.py
+++ credentials.py
@@ -1,15 +1,15 @@
 class Credentials:
 	def credentials(self):
 		return {
-			'aws_bucket': 'bucket-for-testing2023',
-			'aws_access_key_id': 'AKIAXS4H3IU6NLWLNVEI',
-			'aws_secret_access_key': '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',
-			'smtp_account': 'tersk.bo@gmail.com',
-			'smtp_password': 'fxfqvhiakzdwlonu',
-			'db_host': 'database-djinni.cvh4mhjw2u7i.us-east-1.rds.amazonaws.com',
-			'db_login': 'bohdanterskow',
-			'db_password': 'Tibisi1998',
-			'db_name': 'shop_first',
-			'telegram_key': '6957518695:AAH-Ru8OInVCYcvfa3DSaVEbFoengMmCYTU',
-			'telegram_chat_id': '528871947'
+			'aws_bucket': '',
+			'aws_access_key_id': '',
+			'aws_secret_access_key': '',
+			'smtp_account': '',
+			'smtp_password': '',
+			'db_host': '',
+			'db_login': '',
+			'db_password': '',
+			'db_name': '',
+			'telegram_key': '',
+			'telegram_chat_id': ''
 		}
